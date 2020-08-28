a309c9f3645e887fa6a3659325734f6e9d5b37e9
nagln <laxminagln@gmail.com>
2020-08-28 14:30:46 +0530

    Updates

---
+++ precache-manifest.89b28d403e316c66f2dcf4a2622fe5f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fd6b3c3cd885f350ed91b0e2ec77040",
+    "url": "/exercise-tracker/index.html"
+  },
+  {
+    "revision": "9074a788bc288613f2e6",
+    "url": "/exercise-tracker/static/css/2.b56a0318.chunk.css"
+  },
+  {
+    "revision": "2006e000df54d21a44e5",
+    "url": "/exercise-tracker/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9074a788bc288613f2e6",
+    "url": "/exercise-tracker/static/js/2.6059273f.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/exercise-tracker/static/js/2.6059273f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2006e000df54d21a44e5",
+    "url": "/exercise-tracker/static/js/main.8972b20c.chunk.js"
+  },
+  {
+    "revision": "19efbaa7935c6d4a6ff8",
+    "url": "/exercise-tracker/static/js/runtime-main.c3c5cc0f.js"
+  }
+]);
