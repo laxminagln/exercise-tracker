120ceee0f403e1f9267bfe1c82d3ba3d6fcc47bf
nagln <laxminagln@gmail.com>
2020-08-28 12:41:55 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css"
 
 function App() {
   return (
-    <div className="App">
-      Hello world!
-    </div>
+    <Router>
+      <Navbar />
+      <br/>
+      <Route path="/" exact component={ExerciseList} />
+      <Route path="/edit/:id" exact component={EditExercise} />
+      <Route path="/create" exact component={CreateExercise} />
+      <Route path="/user" exact component={CreateUser} />
+    </Router>
   );
 }
 
